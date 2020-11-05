7727bd148e2eed867be5536f2ab00bdab1d247d3
Tommi Juslin <tommi.juslin@gmail.com>
2020-11-05 16:40:40 +0200

    unicafe maksukorttitestit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(1000);
+        assertEquals("saldo: 10.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.lataaRahaa(1000);
+        kortti.otaRahaa(400);
+        assertEquals("saldo: 6.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahatRiittivat() {
+        assertTrue(kortti.otaRahaa(10));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosRahatEiRiittaneet() {
+        assertFalse(kortti.otaRahaa(100));
+    }
+    
+    @Test
+    public void saldoMetodiToimiiOikein() {
+        assertTrue(kortti.saldo() == 10);
+    }
 }
